810d784eb5121e726699143ed8d5ad219a229f15
BobTheBuilder365 <47754209+BobTheBuilder365@users.noreply.github.com>
2020-07-05 09:17:12 +0200

    Grundstruktur des Models - ohne Methoden

---
+++ src/tictactoe/TicTacToe_Model.java
@@ -0,0 +1,25 @@
+package tictactoe;
+
+public class TicTacToe_Model {
+	public static final int BOARD_SIZE = 3; // Game board size
+	
+	public enum ValidMove { X, O }; // Possible moves
+	private ValidMove nextMove = ValidMove.X; //Next Move
+	
+	private ValidMove winner = null; // Set to the winner, if there is one
+	
+	protected ValidMove[][] board = new ValidMove[BOARD_SIZE][BOARD_SIZE]; // Game board
+	
+	// Method to make the game move
+//	public boolean makeMove(int col, int row) {
+//		boolean moveMade = false;
+//		
+//		
+//		
+//		
+//		return moveMade;
+//	}
+	
+	// Metho to check if someone has one
+	
+}
