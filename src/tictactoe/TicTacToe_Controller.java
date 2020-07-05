9c24c9fdb24dadd978782abe8637e5c5b5c02859
BobTheBuilder365 <47754209+BobTheBuilder365@users.noreply.github.com>
2020-07-05 09:22:21 +0200

    Grundstruktur des Controllers ohne Methode(n)

---
+++ src/tictactoe/TicTacToe_Controller.java
@@ -0,0 +1,25 @@
+package tictactoe;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+
+public class TicTacToe_Controller {
+	private TicTacToe_Model model;
+	private TicTacToe_View view;
+
+	public TicTacToe_Controller(TicTacToe_Model model, TicTacToe_View view) {
+		this.model = model;
+		this.view = view;
+		
+		for (int col = 0; col < view.buttons.length; col++) {
+			for (int row = 0; row < view.buttons[0].length; row++) {
+				view.buttons[col][row].setOnAction(this::buttonClick);
+			}
+		}
+	}
+	private void buttonClick(ActionEvent e) {
+		// Find the button
+	}
+}
