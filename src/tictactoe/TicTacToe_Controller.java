fb290c40ec4bb15377567a4a63d66f2b0fb3ba6d
BobTheBuilder365 <wym.office@gmail.com>
2020-07-26 16:55:52 +0200

    model und controller verbunden

--- src/tictactoe/TicTacToe_Controller.java
+++ src/tictactoe/TicTacToe_Controller.java
@@ -19,8 +19,29 @@ public class TicTacToe_Controller {
 			}
 		}
 	}
-	// TODO
+	
 	private void buttonClick(ActionEvent e) {
-		// Find the button (Auf welchen Button hat der User gedrückt?)
+		// Finde den Button - Auf welchen Button hat der User gedrückt?)
+		Button btn = (Button) e.getSource();
+		int col = -1;
+		int row = -1;
+		for (int c = 0; c < view.buttons.length; c++) {
+			for (int r = 0; r < view.buttons[0].length; r++) {
+				if (btn == view.buttons[c][r]) {
+					col = c;
+					row = r;
+				}
+			}
+		}
+
+		if (model.makeMove(col, row)) {
+			view.buttons[col][row].setText(model.board[col][row].toString());
+			if (model.getWinner() != null) {
+				Alert showWinner = new Alert(AlertType.INFORMATION);
+				showWinner.setTitle("Winner");
+				showWinner.setHeaderText("The winner is " + model.getWinner());
+				showWinner.showAndWait();
+			}
+		}
 	}
 }
--- src/tictactoe/TicTacToe_Model.java
+++ src/tictactoe/TicTacToe_Model.java
@@ -1,6 +1,8 @@
 package tictactoe;
 
+
 public class TicTacToe_Model {
+	
 	public static final int BOARD_SIZE = 3; // Game board size
 	
 	public enum ValidMove { X, O }; // Possible moves
@@ -10,16 +12,48 @@ public class TicTacToe_Model {
 	
 	protected ValidMove[][] board = new ValidMove[BOARD_SIZE][BOARD_SIZE]; // Game board
 	
-	// Method to make the game move
-//	public boolean makeMove(int col, int row) {
-//		boolean moveMade = false;
-//		
-//		
-//		
-//		
-//		return moveMade;
-//	}
+	/**
+	 * Method to make the game move
+	 * Make a move in the indicated place, alternating X and O.
+	 * We can only move in a blank (null) space.
+	 */
+	public boolean makeMove(int col, int row) {
+		boolean moveMade = false;
+		if (winner == null && board[col][row] == null) {
+			board[col][row] = nextMove;
+			nextMove = (nextMove == ValidMove.X) ? ValidMove.O : ValidMove.X;
+			moveMade = true;
+		}
+		checkWinner();
+		return moveMade;
+	}
 	
-	// Method to check if someone has one
+	/**
+	 * Method to check if someone has one
+	 * Set the result if someone has won
+	 */
+	private void checkWinner() {
+		for (int i = 0; i < BOARD_SIZE && winner == null; i++) {
+			boolean rowWinner  = true;
+			boolean colWinner = true;
+			for (int j = 1; j < BOARD_SIZE; j++) {
+				rowWinner &= board[i][0] != null && board[i][0] == board[i][j];
+				colWinner &= board[0][i] != null && board[0][i] == board[j][i];
+			}
+			if (rowWinner) winner = board[i][0];
+			if (colWinner) winner = board[0][i];
+		}
+		boolean diag1Winner = true;
+		boolean diag2Winner = true;
+		for (int i = 1; i < BOARD_SIZE; i++) {
+			diag1Winner &= board[0][0] != null && board[0][0] == board[i][i];
+			diag2Winner &= board[0][BOARD_SIZE-1] != null && board[0][BOARD_SIZE-1] == board[i][BOARD_SIZE-1-i];
+		}
+		if (diag1Winner) winner = board[0][0];
+		if (diag2Winner) winner = board[0][BOARD_SIZE-1];
+	}
 	
+	public ValidMove getWinner() {
+		return winner;
+	}
 }
